de1aeecf3766bc380f7c4c029a2f56f6d23d8f63
Taro <chentao@fanxiapp.com>
2022-01-19 15:07:12 +0800

    comit

--- src/component/Card/AnchorBlank.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AnchorBlank({ to, children, className }) {
-  return (
-    <a href={to} target="_blank" rel="noreferrer" className={className}>
-      {children}
-    </a>
-  );
-}
